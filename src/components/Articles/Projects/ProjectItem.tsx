9455a0eba326287aae7c841ca655d49192a1157a
Michel Barakat <michel.barakat@bertelsmann.de>
2024-03-20 12:03:44 +0100

    remove warning from old nextjs image

--- src/components/Articles/Projects/ProjectItem.tsx
+++ src/components/Articles/Projects/ProjectItem.tsx
@@ -16,10 +16,9 @@ const ProjectItem: React.FC<Project> = ({
     <article className=" gap-4 border-t-2 border-neutral-6 py-6 first-of-type:border-none last-of-type:pb-0 sm:flex">
       <div className="mb-2 sm:w-1/3">
         <Image
-          className="rounded-lg border-4 border-yellow-500"
+          className="rounded-lg border-4 border-yellow-500 w-full"
           src={`/images/${slugify(title).toLowerCase()}.jpg`}
           alt={title}
-          layout="responsive"
           width={200}
           height={200}
         />
